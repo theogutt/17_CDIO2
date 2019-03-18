66693f39fbc84936342748852900c175a0b56509
Jonatan <s185096@student.dtu.dk>
2019-03-18 16:03:09 +0100

    Lavet command metoder

--- src/main/java/VægtController.java
+++ src/main/java/VægtController.java
@@ -1,2 +1,35 @@
 public class VægtController {
+
+
+
+    public VægtController(){
+    }
+
+
+    public String commandS(){
+        return "S crlf";
+    }
+
+    public String commandT(){
+        return "T crlf";
+    }
+
+    public String commandD(String output){
+        return "D " + "\"" + output + "\"" + " crlf";
+    }
+
+    public String commandDW(){
+        return "DW crlf";
+    }
+
+    public String commandP111(String output){
+        if (output.length() <= 30)
+            return "P111 " + "\"" + output + "\"" + " crlf";
+        else
+            return "FEJL, for langt output";
+    }
+
+    public String commandRM20(String output){
+        return "RM20 8" + "\"" + "INDSKRIV NR" + "\"" + " \"\"" + " \"&3\" " + "crlf";
+    }
 }
