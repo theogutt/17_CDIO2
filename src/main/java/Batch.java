e413ad139da4f9dfc886c0f547644cdf7364cd24
Andreas <mail.Tailspot@gmail.com>
2019-03-18 15:25:54 +0100

    Batch klasse lave -Andreas

--- src/main/java/Batch.java
+++ src/main/java/Batch.java
@@ -1,2 +1,26 @@
 public class Batch {
+
+    private int batchID;
+    private String batchName;
+
+    public Batch(int batchID, String batchName){
+        this.batchID = batchID;
+        this.batchName = batchName;
+    }
+
+    public int getBatchID(){
+        return this.batchID;
+    }
+
+    public String getBatchName(){
+        return this.batchName;
+    }
+
+    public void setBatchID(int batchID) {
+        this.batchID = batchID;
+    }
+
+    public void setBatchName(String batchName) {
+        this.batchName = batchName;
+    }
 }
