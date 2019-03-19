3d449db64280cd20c62bc4a15a7496bef23b6522
theogutt <theodorguttesen@gmail.com>
2019-03-19 13:25:10 +0100

    tilføjede trin, beskrevet i opgaven. Mangler kode til at kvittere.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,7 @@
 public class Main {
     public static void main(String []args ) throws Exception {
         TUI tui = new TUI();
+        //tui.afvejningsProcedure();
         while(true) {
             tui.commandMenu();
         }
--- src/main/java/TUI.java
+++ src/main/java/TUI.java
@@ -1,8 +1,13 @@
+import java.io.IOException;
 import java.util.Scanner;
 
 public class TUI {
     private VægtController v;
+    private Bruger bruger;
+    private Batch batch;
     public TUI() throws Exception {
+        bruger = new Bruger(12, "Anders And");
+        batch = new Batch(1234, "Salt");
         v = new VægtController();
     }
     public void commandMenu() throws Exception {
@@ -23,5 +28,46 @@ public class TUI {
         System.out.println("Indtast ord: ");
         String output = sc.next();
         return output;
+    }
+    public void afvejningsProcedure() throws IOException {
+        //trin 1
+        v.metoder(v.commandD("Indtast operatørnummer"));
+        //trin 2
+        v.metoder(v.commandP111(output()));
+        //trin 3
+        v.metoder(v.commandD(bruger.getNavn()));
+        //trin 4
+        v.metoder(v.commandD("Indtast batchnummer"));
+        //trin 5
+        v.metoder(v.commandP111(output()));
+        //trin 6 - operatøren kvitterer for at navnet er korrekt
+        //trin 7
+        v.metoder(v.commandT());
+        //trin 8 - operatøren kvitterer
+        //trin 9 - vægten er blevet tareret
+        //trin 10
+        v.metoder(v.commandD("Placer tara"));
+        //trin 11 - operatøren kvitterer
+        //trin 12
+        v.metoder(v.commandS());
+        //trin 13
+        v.metoder(v.commandT());
+        //trin 14
+        v.metoder(v.commandD("Placer netto på vægten"));
+        //trin 15 - operatøren kvitterer
+        //trin 16
+        v.metoder(v.commandS());
+        //trin 17
+        v.metoder(v.commandT());
+        //trin 18
+        v.metoder(v.commandD("Fjern brutto fra vægten"));
+        //trin 19 - operatøren kvitterer
+        //trin 20
+        v.metoder(v.commandS());
+        //trin 21
+        v.metoder(v.commandD("OK"));
+        //trin 22 - operatøren kvitterer
+        //trin 23
+        v.metoder(v.commandT());
     }
 }
