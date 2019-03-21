8e13dc617f7d9dcb9b03671308e413eac75e9200
Your Name <you@example.com>
2019-03-21 15:18:33 +0100

    Tilføjet forfatter navne

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,12 @@
+/*
+Lavet af Gruppe 17              d. 23-03-2019
+Aleksander Lægsgaard Jørgensen  s185118
+Jonatan Amtoft Dahl             s185096
+Søren Hother Rasmussen          s185103
+Andreas Østergaard Schliemann   s175561
+Josephine Weirøse               s164177
+Theodor Peter Guttesen          s185121
+*/
 public class Main {
     public static void main(String []args ) throws Exception {
         Controller c = new Controller();
